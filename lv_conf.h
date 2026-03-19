9e10f254250617fbc6aea113bb909582eb889c4a
ATrain80 <101676491+ATrain80@users.noreply.github.com>
2026-03-19 11:33:12 +0100

    Create lv_conf.h

---
+++ lv_conf.h
@@ -0,0 +1,24 @@
+#ifndef LV_CONF_H
+#define LV_CONF_H
+
+/* ========== BASIC SETTINGS ========== */
+#define LV_HOR_RES_MAX          1024
+#define LV_VER_RES_MAX          600
+#define LV_COLOR_DEPTH          16
+
+/* ========== FEATURE ENABLING ========== */
+#define LV_USE_ANIMATION        1
+#define LV_USE_GROUP            0
+#define LV_USE_GPU              0
+#define LV_USE_FILESYSTEM       0
+#define LV_USE_USER_DATA        1
+#define LV_USE_ARC               1
+#define LV_USE_BTN               1
+#define LV_USE_LABEL             1
+#define LV_USE_LINE              1
+#define LV_USE_IMG               0
+#define LV_USE_OBJ               1
+#define LV_USE_CONT              1
+#define LV_USE_FLEX              1
+
+#endif /*LV_CONF_H*/
